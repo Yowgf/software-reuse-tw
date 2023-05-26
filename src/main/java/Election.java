6edd4078e10ae8d474945b4e2cb3882b6ebcc320
yowgf <alexthomasmol@gmail.com>
2023-05-25 22:50:44 -0300

    Make candidate id a class

--- src/main/java/Election.java
+++ src/main/java/Election.java
@@ -10,7 +10,7 @@ public class Election {
 
   private boolean finished;
   private ElectionResult result = new ElectionResult();
-  private Map<String, Candidate> candidates = new HashMap<String, Candidate>();
+  private Map<CandidateID, Candidate> candidates = new HashMap<CandidateID, Candidate>();
 
   public static class Builder {
     protected String password;
@@ -49,8 +49,8 @@ public class Election {
   }
 
   // addVote handles a vote for some candidate.
-  public boolean addVote(CandidateType candidateType, String location, String candidateNumber) {
-    String candidateId = composeCandidateId(candidateType, location, candidateNumber);
+  public boolean addVote(CandidateType typ, String location, String candidateNumber) {
+    var candidateId = new CandidateID(typ, location, candidateNumber);
     System.err.println("Adding vote to candidate " + candidateId);
     boolean exists = candidates.containsKey(candidateId);
     if (!exists) {
@@ -61,12 +61,12 @@ public class Election {
   }
 
   public void addNullVote(CandidateType candidateType, String location) {
-    String candidateId = composeCandidateId(candidateType, location, "");
+    var candidateId = new CandidateID(candidateType, location, 0);
     result.addNullVote(candidateId);
   }
 
   public void addProtestVote(CandidateType candidateType, String location) {
-    String candidateId = composeCandidateId(candidateType, location, "");
+    var candidateId = new CandidateID(candidateType, location, 0);
     result.addProtestVote(candidateId);
   }
 
@@ -85,21 +85,20 @@ public class Election {
   }
 
   public void addCandidate(Candidate candidate) {
-    String candidateId =
-        composeCandidateId(
-            candidate.getType(), candidate.getLocation(), String.valueOf(candidate.getNumber()));
+    var candidateId = new CandidateID(
+            candidate.getType(), candidate.getLocation(), candidate.getNumber());
     System.err.println("Adding candidate with id " + candidateId);
     candidates.put(candidateId, candidate);
   }
 
-  public Candidate getCandidateByNumber(CandidateType candidateType, String location, int num) {
-    return getCandidateByNumber(candidateType, location, String.valueOf(num));
+    // Overload of getCandidate
+  public Candidate getCandidate(CandidateType candidateType, String location, int num) {
+    return getCandidate(candidateType, location, String.valueOf(num));
   }
 
-  public Candidate getCandidateByNumber(
+  public Candidate getCandidate(
       CandidateType candidateType, String location, String candidateNumber) {
-    String candidateId = composeCandidateId(candidateType, location, candidateNumber);
-    return candidates.get(candidateId);
+    return candidates.get(new CandidateID(candidateType, location, candidateNumber));
   }
 
   public String getResults(String password) {
@@ -112,146 +111,9 @@ public class Election {
     var federalDeputyRank = new ArrayList<FederalDeputy>();
 
     var builder = new StringBuilder();
-
     builder.append("Resultado da eleicao:\n");
-
-    // int totalVotesP = presidentProtestVotes + nullPresidentVotes;
-    // for (Map.Entry<Integer, President> candidateEntry : presidentCandidates.entrySet()) {
-    //   President candidate = candidateEntry.getValue();
-    //   totalVotesP += candidate.numVotes;
-    //   presidentRank.add(candidate);
-    // }
-
-    // int totalVotesFD = federalDeputyProtestVotes + nullFederalDeputyVotes;
-    // for (Map.Entry<String, FederalDeputy> candidateEntry : federalDeputyCandidates.entrySet()) {
-    //   FederalDeputy candidate = candidateEntry.getValue();
-    //   totalVotesFD += candidate.numVotes;
-    //   federalDeputyRank.add(candidate);
-    // }
-
-    // var sortedFederalDeputyRank =
-    //     federalDeputyRank.stream()
-    //         .sorted((o1, o2) -> o1.numVotes == o2.numVotes ? 0 : o1.numVotes < o2.numVotes ? 1 :
-    // -1)
-    //         .collect(Collectors.toList());
-
-    // var sortedPresidentRank =
-    //     presidentRank.stream()
-    //         .sorted((o1, o2) -> o1.numVotes == o2.numVotes ? 0 : o1.numVotes < o2.numVotes ? 1 :
-    // -1)
-    //         .collect(Collectors.toList());
-
-    // builder.append("  Votos presidente:\n");
-    // builder.append("  Total: " + totalVotesP + "\n");
-    // builder.append(
-    //     "  Votos nulos: "
-    //         + nullPresidentVotes
-    //         + " ("
-    //         + decimalFormater.format((double) nullPresidentVotes / (double) totalVotesFD * 100)
-    //         + "%)\n");
-    // builder.append(
-    //     "  Votos brancos: "
-    //         + presidentProtestVotes
-    //         + " ("
-    //         + decimalFormater.format((double) presidentProtestVotes / (double) totalVotesFD *
-    // 100)
-    //         + "%)\n");
-    // builder.append("\tNumero - Partido - Nome  - Votos  - % dos votos totais\n");
-    // for (President candidate : sortedPresidentRank) {
-    //   builder.append(
-    //       "\t"
-    //           + candidate.number
-    //           + " - "
-    //           + candidate.party
-    //           + " - "
-    //           + candidate.name
-    //           + " - "
-    //           + candidate.numVotes
-    //           + " - "
-    //           + decimalFormater.format((double) candidate.numVotes / (double) totalVotesP * 100)
-    //           + "%\n");
-    // }
-
-    // President electPresident = sortedPresidentRank.get(0);
-    // builder.append("\n\n  Presidente eleito:\n");
-    // builder.append(
-    //     "  "
-    //         + electPresident.name
-    //         + " do "
-    //         + electPresident.party
-    //         + " com "
-    //         + decimalFormater.format((double) electPresident.numVotes / (double) totalVotesP *
-    // 100)
-    //         + "% dos votos\n");
-    // builder.append("\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n");
-
-    // builder.append("\n\n  Votos deputado federal:\n");
-    // builder.append(
-    //     "  Votos nulos: "
-    //         + nullFederalDeputyVotes
-    //         + " ("
-    //         + decimalFormater.format((double) nullFederalDeputyVotes / (double) totalVotesFD *
-    // 100)
-    //         + "%)\n");
-    // builder.append(
-    //     "  Votos brancos: "
-    //         + federalDeputyProtestVotes
-    //         + " ("
-    //         + decimalFormater.format(
-    //             (double) federalDeputyProtestVotes / (double) totalVotesFD * 100)
-    //         + "%)\n");
-    // builder.append("  Total: " + totalVotesFD + "\n");
-    // builder.append("\tNumero - Partido - Nome - Estado - Votos - % dos votos totais\n");
-    // for (FederalDeputy candidate : sortedFederalDeputyRank) {
-    //   builder.append(
-    //       "\t"
-    //           + candidate.number
-    //           + " - "
-    //           + candidate.party
-    //           + " - "
-    //           + candidate.state
-    //           + " - "
-    //           + candidate.name
-    //           + " - "
-    //           + candidate.numVotes
-    //           + " - "
-    //           + decimalFormater.format((double) candidate.numVotes / (double) totalVotesFD * 100)
-    //           + "%\n");
-    // }
-
-    // FederalDeputy firstDeputy = sortedFederalDeputyRank.get(0);
-    // FederalDeputy secondDeputy = sortedFederalDeputyRank.get(1);
-    // builder.append("\n\n  Deputados eleitos:\n");
-    // builder.append(
-    //     "  1º "
-    //         + firstDeputy.name
-    //         + " do "
-    //         + firstDeputy.party
-    //         + " com "
-    //         + decimalFormater.format((double) firstDeputy.numVotes / (double) totalVotesFD * 100)
-    //         + "% dos votos\n");
-    // builder.append(
-    //     "  2º "
-    //         + secondDeputy.name
-    //         + " do "
-    //         + secondDeputy.party
-    //         + " com "
-    //         + decimalFormater.format((double) secondDeputy.numVotes / (double) totalVotesFD *
-    // 100)
-    //         + "% dos votos\n");
+    builder.append(result.prettyString());
 
     return builder.toString();
-  }
-
-  private String composeCandidateId(
-      CandidateType candidateType, String location, String candidateNumber) {
-    String s = candidateType.name;
-    if (!location.isEmpty() && candidateType.isLocationSensitive()) {
-      s += "_" + location;
-    }
-    if (!candidateNumber.isEmpty()) {
-      s += "_" + candidateNumber;
-    }
-    return s;
   }
 }
